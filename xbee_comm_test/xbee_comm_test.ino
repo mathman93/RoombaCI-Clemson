f27d36010d0d2b634bacca88662f99041baf556f
jconley97 <jconley@g.clemson.edu>
2017-04-27 11:54:16 -0400

    Update xbee_comm_test.ino

--- xbee_comm_test/xbee_comm_test.ino
+++ xbee_comm_test/xbee_comm_test.ino
@@ -2,15 +2,15 @@
 // Input data via the serial monitor and it will send the data you inputted out to the other xbees
 // Data sent from other xbees will be received and printed out to the serial monitor
 #include <SoftwareSerial.h>
-// XBee's DOUT (TX, pin 2 of xbee) is connected to pin 2 of Arduino board (Arduino's Software RX)
+// XBee's DOUT (TX, pin 2 of xbee) is connected to pin 11 of Arduino board (Arduino's Software RX)
 // XBee's DIN (RX, pin 3 of xbee) is connected to pin 12 of Arduino board (Arduino's Software TX)
-SoftwareSerial XBee(2, 12); // RX, TX
+SoftwareSerial XBee(11, 12); // RX, TX
 
 void setup()
 {
-  // set up baud rates for XBee and Serial monitor
-  XBee.begin(57600);
-  Serial.begin(115200);
+  // set up baud rates for XBee and Serial monitor, they can be different as long as all Xbees are configured to same baud rate
+  XBee.begin(57600); // we found this to be the max baud rate of the Xbees, set all Xbee modules to this baud rate
+  Serial.begin(115200); // set the Serial monitor to this buad rate
 }
 
 void loop()
