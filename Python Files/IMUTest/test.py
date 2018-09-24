d4ed253580d966ad6a09519dd6174622ec07a47f
Josh Harvey <jwharve@g.clemson.edu>
2018-09-24 14:55:35 -0400

    testing file

---
+++ Python Files/IMUTest/test.py
@@ -0,0 +1,11 @@
+import RoombaCI_lib
+
+Roomba = RoombaCI_lib.Create_2("/dev/ttyS0", 115200)
+
+imu = RoombaCI_lib.LSM9DS1_IMU() # Initialize IMU
+
+for ii in range(500):
+    reading[ii] = imu.ReadMagRaw
+
+for ii in range(500):
+    print(reading[ii])
