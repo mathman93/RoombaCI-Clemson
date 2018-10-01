0165351b41ebff9492904c85116d97104f3e3eba
Josh Harvey <jwharve@g.clemson.edu>
2018-10-01 14:53:22 -0400

    updated mag testing file

--- Python Files/test.py
+++ Python Files/test.py
@@ -8,36 +8,75 @@ imu = RoombaCI_lib.LSM9DS1_IMU() # Initialize IMU
 
 LENGTH = 500
 
-readingX = [0 for i in range(LENGTH)]
-readingY = [0 for i in range(LENGTH)]
-readingZ = [0 for i in range(LENGTH)]
+magX = [0 for i in range(LENGTH)]
+magY = [0 for i in range(LENGTH)]
+magZ = [0 for i in range(LENGTH)]
 
-dictionary = {
-    "readingXdict" : [],
-    "readingYdict" : [],
-    "readingZdict" : []
-}
+accelX = [0 for i in range(LENGTH)]
+accelY = [0 for i in range(LENGTH)]
+accelZ = [0 for i in range(LENGTH)]
 
-def addValue(key, value):
-    dictionary[key].append(value)
-
-def writeToFile():
-    with open('results.json','w') as fp:
-        json.dump(dictionary,fp,separators=(',', ': '))
-
-imu.CalibrateMag()
+gyroX = [0 for i in range(LENGTH)]
+gyroY = [0 for i in range(LENGTH)]
+gyroZ = [0 for i in range(LENGTH)]
 
 for ii in range(LENGTH):
-    [a, b, c] = imu.ReadMag()
-    # a.addValue("readingX")
-    # b.addValue("readingY")
-    # c.addValue("readingZ")
-    readingX[ii] = a
-    readingY[ii] = b
-    readingZ[ii] = c
+    [a, b, c] = imu.ReadMagRaw()
+    magX[ii] = a
+    magY[ii] = b
+    magZ[ii] = c
+	
+	[a, b, c] = ReadAccelRaw()
+	accelX = a
+	accelY = b
+	accelZ = c
+	
+	[a, b, c] = ReadGyroRaw()
+	gyroX = a
+	gyroY = b
+	gyroZ = c
 
-addValue("readingXdict",readingX)
-addValue("readingYdict",readingY)
-addValue("readingZdict",readingZ)
+print("magX")
+for ii in range(LENGTH)
+	print("{:.3f}".format(magX[ii]))
+	print(", ")
 
-writeToFile()
+print("magY")
+for ii in range(LENGTH)
+	print("{:.3f}".format(magY[ii]))
+	print(", ")
+
+print("magZ")
+for ii in range(LENGTH)
+	print("{:.3f}".format(magZ[ii]))
+	print(", ")
+
+print("accelX")
+for ii in range(LENGTH)
+	print("{:.3f}".format(accelX[ii]))
+	print(", ")
+
+print("accelY")
+for ii in range(LENGTH)
+	print("{:.3f}".format(accelY[ii]))
+	print(", ")
+
+print("accelZ")
+for ii in range(LENGTH)
+	print("{:.3f}".format(accelZ[ii]))
+	print(", ")
+
+print("gyroX")
+for ii in range(LENGTH)
+	print("{:.3f}".format(gyroX[ii]))
+	print(", ")
+
+print("gyroY")
+for ii in range(LENGTH)
+	print("{:.3f}".format(gyroY[ii]))
+	print(", ")
+
+print("gyroZ")
+for ii in range(LENGTH)
+	print("{:.3f}".format(gyroZ[ii]))
+	print(", ")
