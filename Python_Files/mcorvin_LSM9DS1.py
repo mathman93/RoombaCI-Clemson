b221404ea31c7b6c4c9cbf932e0bab6511c4f94b
Timothy Anglea <mathman93@gmail.com>
2019-04-11 15:34:12 -0400

    fix the tabs

--- Python_Files/mcorvin_LSM9DS1.py
+++ Python_Files/mcorvin_LSM9DS1.py
@@ -150,7 +150,7 @@ class I2CDevice(Lockable):
 
 			try:
 				self._i2c_bus = smbus2.SMBus(bus_num)
-                                print("_i2c_bus made")
+				print("_i2c_bus made")
 			except FileNotFoundError:
 				raise RuntimeError("I2C Bus #%d not found!" % bus_num)
 
