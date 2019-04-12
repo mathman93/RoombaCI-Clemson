0e31997f4c7a0454597a96266c15aa58dc800d2a
Madeline Corvin <mcorvin@g.clemson.edu>
2019-04-12 10:40:14 -0400

    Fixes to temp functions

--- Python_Files/mcorvin_LSM9DS1.py
+++ Python_Files/mcorvin_LSM9DS1.py
@@ -377,8 +377,7 @@ class LSM9DS1_I2C(I2CDevice):
         # read temp sensor
         buf = self._read_bytes(_XGTYPE, _LSM9DS1_REGISTER_TEMP_OUT_L, 2)
         temp = ((buf[1] << 8) | (buf[0]) >> 4
-        ret_temp = _twos_comp(temp,12)
-        return ret_temp
+        return (_twos_comp(temp,12))
 
     @property
     def temperature(self):
