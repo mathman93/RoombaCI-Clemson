cdad07790488b0e42141d64f651836bb2968c23d
Madeline Corvin <mcorvin@g.clemson.edu>
2019-04-18 15:27:04 -0400

    Deleted Lockable class (not used)

--- Python_Files/mcorvin_LSM9DS1.py
+++ Python_Files/mcorvin_LSM9DS1.py
@@ -115,25 +115,6 @@ class ContextManaged:
         return
 
 
-#class Lockable(ContextManaged):
-    # An object that must be locked to prevent collisions on a microcontroller resource.
-#    _locked = False
-
-#    def try_lock(self):
-        # Attempt to grab the lock. Return True on success, False if the lock is already taken.
-#        if self._locked:
-#            return False
-#        self._locked = True
-#        return True
-
-#    def unlock(self):
-        # Release the lock so others may use the resource.
-#        if self._locked:
-#            self._locked = False
-#        else:
-#            raise ValueError("Not locked")
-
-
 class I2CDevice(ContextManaged):
     MASTER = 0
     SLAVE = 1
