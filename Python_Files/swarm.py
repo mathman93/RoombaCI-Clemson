2a1d0cee3c070e9ea1d3698d85d658e45b105f4d
jasonClemson <52469304+jasonClemson@users.noreply.github.com>
2019-07-03 16:10:32 -0400

    Test Reset

--- Python_Files/swarm.py
+++ Python_Files/swarm.py
@@ -36,13 +36,13 @@ DisplayDateTime() # Display current date and time
 GPIO.setup(yled, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(rled, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(gled, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(micOne, GPIO.IN, pull_up_down= GPIO.PUD_DOWN)##Check for initial later
-GPIO.setup(micTwo, GPIO.IN)
-GPIO.setup(micThree, GPIO.IN)
+GPIO.setup(micOne, GPIO.IN, pull_up_down= GPIO.PUD_UP)##Check for initial later
+GPIO.setup(micTwo, GPIO.IN,  pull_up_down= GPIO.PUD_UP)
+GPIO.setup(micThree, GPIO.IN, pull_up_down= GPIO.PUD_UP)
 GPIO.setup(reset, GPIO.OUT, initial=GPIO.LOW)
 
 
-GPIO.output(reset,GPIO.HIGH)
+GPIO.output(reset,GPIO.LOW)
 while True:
 	try:
 		statusOne=GPIO.input(micOne)
@@ -51,6 +51,7 @@ while True:
 		print("Mic One: {0}".format(statusOne))
 		print("Mic Two: {0}".format(statusTwo))
 		print("Mic Three: {0}".format(statusThree))
+		
 		
 	except KeyboardInterrupt:
 		break
