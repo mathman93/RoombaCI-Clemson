4a5074c78b6a8e7e387d3fe4791256c43999e3ef
jasonClemson <52469304+jasonClemson@users.noreply.github.com>
2019-07-15 10:51:44 -0400

    Update song_final.py

--- Python_Files/song_final.py
+++ Python_Files/song_final.py
@@ -56,7 +56,7 @@ def makeNoise():
 #makeNoise()
     # SuperStar Theme (16 notes)
 while True:
-    try:
+    #try:
         note1=int(input("First Note:"))
         note2=int(input("Second Note:"))
         note3=int(input("Third Note:"))
@@ -83,7 +83,7 @@ while True:
         Roomba.DirectWrite(0)
         GPIO.output(gled, GPIO.HIGH)
         time.sleep(1)
-    except KeyboardInterrupt:
-        break
+    #except KeyboardInterrupt:
+     #   break
 Roomba.ShutDown()
 GPIO.cleanup()
