dbf7dbf05cb7ff72c03639867054d6637fa99f30
jasonClemson <52469304+jasonClemson@users.noreply.github.com>
2019-07-18 11:14:48 -0400

    Update swarm3.0.py

--- Python_Files/swarm3.0.py
+++ Python_Files/swarm3.0.py
@@ -121,7 +121,7 @@ while 0 in times:
 GPIO.output(gled,GPIO.LOW)
 GPIO.output(yled,GPIO.LOW)
 GPIO.output(rled,GPIO.LOW)
-if max(times)-min(times)>0.001:
+if max(times)-min(times)>0.002:
     print("Nah fam")
     times=[0,0,0]
 else:
