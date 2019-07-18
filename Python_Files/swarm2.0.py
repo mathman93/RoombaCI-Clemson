70428d2f6ba75fa6799897bd1b5c8ce2cd6ddd4f
jasonClemson <52469304+jasonClemson@users.noreply.github.com>
2019-07-18 09:42:09 -0400

    Update swarm2.0.py

--- Python_Files/swarm2.0.py
+++ Python_Files/swarm2.0.py
@@ -26,6 +26,7 @@ reset=24
 oneNotHeard=True
 twoNotHeard=True
 threeNotHeard=True
+notHeards=[True,True,True]
 lastHeard=-1
 statusOne=0
 statusTwo=0
@@ -58,9 +59,12 @@ def triangulate(t12,t23,t13,c):
     a13=0.5*343*t13
     b13=math.sqrt(c**2-a13**2)
     
-def checkMic(notHeard, micStatus):
-    if notHeard and micStatus==1:
-        return False, time.time()
+def checkMic(micStatus, micNum):
+    global notHeards
+    if notHeards[micNum-1] and micStatus==1:
+        global lastHeard
+        lastHeard=time.time()
+        notHeards[micNum-1]=False
         #notHeard=False
         #micTime=time.time()
         #recentHeard=micTime
@@ -106,8 +110,10 @@ while True:
         # print (statusThree)
         global statusOne
         statusOne=GPIO.input(micOne)
-        global statusTwo=GPIO.input(micTwo)
-        global statusThree=GPIO.input(micThree)
+        global statusTwo
+        statusTwo =GPIO.input(micTwo)
+        global statusThree
+        statusThree=GPIO.input(micThree)
         if oneNotHeard and statusOne==1:
             #print("micOne")
             oneNotHeard=False
@@ -144,12 +150,12 @@ while True:
                     print("312")
             #time.sleep(0.5)
             #calculations go here
-            global oneNotHeard=True
-            global twoNotHeard=True
-            global threeNotHeard=True
-            global statusOne=0
-            global statusTwo=0
-            global statusThree=0
+            oneNotHeard=True
+            twoNotHeard=True
+            threeNotHeard=True
+            statusOne=0
+            statusTwo=0
+            statusThree=0
             lastHeard=-1
             timedReset()
         elif (not lastHeard<0) and time.time()-lastHeard>0.005 and (not (oneNotHeard and twoNotHeard and threeNotHeard)):
