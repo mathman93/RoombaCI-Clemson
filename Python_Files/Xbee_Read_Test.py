f8369734b2d06e52411ab15e4735bdb41c2977c4
carrie3712 <55411565+carrie3712@users.noreply.github.com>
2019-11-11 16:56:31 -0500

    Update Xbee_Read_Test.py

--- Python_Files/Xbee_Read_Test.py
+++ Python_Files/Xbee_Read_Test.py
@@ -55,9 +55,9 @@ while True:
 			message = Xbee.read(Xbee.inWaiting()).decode() # Read all data in
 			print(message) # To see what the string representation is
 			coordinate = message.split() # To split the string into x and y coordinates
-			print(coordinate)
-			#absissa = float(coordinate[0])
-			#print(absissa)			
+			#print(coordinate)
+			absissa = float(coordinate[0])
+			print(absissa)			
 			#print(float(message)) # Display message to screen as a number
 		
 		if (time.time() - basetime) > basetime_offset: # If enough time has passed.
