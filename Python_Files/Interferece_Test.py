e40a31d2e6d17985ca5409869b5ebce9597e8be8
Winston Stewart <69232268+kozilek1@users.noreply.github.com>
2020-10-20 11:39:11 -0400

    winston newthing
    afjdklafjdsfafd

---
+++ Python_Files/Interferece_Test.py
@@ -0,0 +1,88 @@
+'''
+
+'''
+
+import serial
+import time
+import RPi.GPIO as GPIO
+import sys
+
+## Variables and Constants ##
+global Xbee # Specifies connection to Xbee
+Xbee = serial.Serial('/dev/ttyUSB0', 115200) # Baud rate should be 115200
+yled = 5
+rled = 6
+gled = 13
+
+## Functions and Definitions ##
+''' Displays current date and time to the screen
+	'''
+def DisplayDateTime():
+	# Month day, Year, Hour:Minute:Seconds
+	date_time = time.strftime("%B %d, %Y, %H:%M:%S", time.gmtime())
+	print("Program run: ", date_time)
+
+## -- Code Starts Here -- ##
+# Setup Code #
+GPIO.setmode(GPIO.BCM) # Use BCM pin numbering for GPIO
+DisplayDateTime()
+
+# LED Pin setup
+GPIO.setup(yled, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(rled, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(gled, GPIO.OUT, initial=GPIO.LOW)
+
+# Main Code #
+if Xbee.inWaiting() > 0:
+	# Clear out Xbee buffer
+	x = Xbee.read(Xbee.inWaiting()).decode()
+	print(x)
+
+sendtime = time.time()
+sendtime_offset = 1.0
+basetime = time.time()
+basetime_offset = 0.5
+roombaname = input('enter roombaname: ')
+
+for line in sys.stdin:
+     
+
+        #variable=input('input letter: ')
+		#if variabe='s':
+        if 'send' == line.rstrip();
+			message1 = input('enter x value: ') # Make this the number  you want to send
+            #make this input
+			message2 = input('enter y value: ')
+            #same with this
+			var = "{0} {1:09.3f} {2:09.3f}".format(roombaname, message1,message2) # Make the string representation of the number
+			Xbee.write(var.encode()) # Send the number over the Xbee
+			sendtime += sendtime_offset # Increase offset for next message
+
+		if Xbee.inWaiting() > 22: # If there is something in the receive buffer
+			message = Xbee.read(Xbee.inWaiting()).decode() # Read all data in
+			print(message) # To see what the string representation is
+			coordinate = message.split() # To split the string into x and y coordinates
+			id = coordinate[0]
+			print(id)
+			absissa = float(coordinate[1])
+			print(absissa)
+			ordinate = float(coordinate[2])
+			print(ordinate)
+
+		if (time.time() - basetime) > basetime_offset: # If enough time has passed.
+			if GPIO.input(gled) == True:  # If the LED is on...
+				GPIO.output(gled, GPIO.LOW)  # turn it off
+			else:
+				GPIO.output(gled, GPIO.HIGH) # otherwise, turn it on.
+			basetime += basetime_offset  # set the next base time
+
+	except KeyboardInterrupt:
+		print('The program has ended')
+		break
+
+## -- Ending Code Starts Here -- ##
+# Make sure this code runs to end the program cleanly
+GPIO.output(gled, GPIO.LOW) # Turn off green LED
+
+Xbee.close()
+GPIO.cleanup() # Reset GPIO pins for next program
