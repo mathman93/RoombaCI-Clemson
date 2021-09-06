e89f2a46294fa1b4cdf90dcd86e6548d5d2cd290
Timothy Anglea <mathman93@gmail.com>
2021-09-06 11:12:57 -0400

    Forgot a file in the code update

--- Python_Files/Roomba_Control_Test.py
+++ Python_Files/Roomba_Control_Test.py
@@ -19,25 +19,14 @@ yled = 5
 rled = 6
 gled = 13
 
-# Roomba Constants
-WHEEL_DIAMETER = 72 # millimeters
-WHEEL_SEPARATION = 235 # millimeters
-WHEEL_COUNTS = 508.8 # counts per revolution
-DISTANCE_CONSTANT = (WHEEL_DIAMETER * math.pi)/(WHEEL_COUNTS) # millimeters/count
-TURN_CONSTANT = (WHEEL_DIAMETER * 180)/(WHEEL_COUNTS * WHEEL_SEPARATION) # degrees/count
+speed_step = 25 # mm/s
 
 ## Functions and Definitions ##
-''' Displays current date and time to the screen
-	'''
-def DisplayDateTime():
-	# Month day, Year, Hour:Minute:Seconds
-	date_time = time.strftime("%B %d, %Y, %H:%M:%S", time.gmtime())
-	print("Program run: ", date_time)
 
 ## -- Code Starts Here -- ##
 # Setup Code #
 GPIO.setmode(GPIO.BCM) # Use BCM pin numbering for GPIO
-DisplayDateTime() # Display current date and time
+RoombaCI_lib.DisplayDateTime() # Display current date and time
 
 # LED Pin setup
 GPIO.setup(yled, GPIO.OUT, initial=GPIO.LOW)
@@ -99,13 +88,9 @@ def MyLoop(stdscr):
 	# Initialize forward and spin values
 	forward = 0
 	spin = 0
-	data_counter = 0
-	distance = 0.0 # total distance travelled (millimeters)
-	x_pos = 0.0 # initial x-direction position (millimeters)
-	y_pos = 0.0 # initial y-direction position (millimeters)
 	
 	# Get initial angle from IMU
-	angle = imu.CalculateHeading()
+	Roomba.heading = imu.CalculateHeading()
 	
 	# Display initial information to the screen
 	stdscr.addstr(0,0,"You are now in control of the Roomba.")
@@ -118,60 +103,21 @@ def MyLoop(stdscr):
 	#Roomba.SendQuery(7,43,44,42,41,45)
 	#while Roomba.Available() == 0:
 	#	pass # Wait for sensor packet values to be returned
-	#bumper_byte, l_counts_current, r_counts_current, l_speed, r_speed, light_bumper = Roomba.ReadQuery(7, 43, 44, 42, 41, 45) # Read new wheel counts
-	bumper_byte, l_counts_current, r_counts_current, l_speed, r_speed, light_bumper = Roomba.Query(7,43,44,42,41,45) # Read initial data
-
+	#bumper_byte, l_counts, r_counts, l_speed, r_speed, light_bumper = Roomba.ReadQuery(7,43,44,42,41,45) # Read new wheel counts
+	bumper_byte, l_counts, r_counts, l_speed, r_speed, light_bumper = Roomba.Query(7,43,44,42,41,45) # Read initial data
+	Roomba.SetWheelEncoderCounts(l_counts, r_counts)
 	# Start Query Data Stream
 	Roomba.StartQueryStream(7,43,44,42,41,45)
 	
-	speed_step = 25 # mm/s
-	query_timer = (1/64) # seconds
-	stop_base = time.time()
 	time_base = time.time()
-	query_base = time.time()
 	while True:
 		try:
 			# Read query stream for specific packets (ReadQueryStream)
-			'''if (time.time() - query_base) > query_timer:
-				bumper_byte, l_counts, r_counts, light_bumper, r_speed, l_speed = Roomba.Query(7,43,44,45,41,42)
-				current_time = (time.time() - time_base) # Current time of data
-				stdscr.move(5,0)
-				stdscr.clrtoeol() # Clear line
-				stdscr.addstr(5,0,"{0:.4f}, {1:0>8b}, {2}, {3}, {4:0>8b}, {5}, {6};".format(current_time, bumper_byte, l_counts, r_counts, light_bumper, l_speed, r_speed))
-				data_counter += 1 # Increment counter for the next data sample
-				query_base += query_timer # Update query_base value for next query
-			'''
 			if Roomba.Available() > 0:
 				bumper_byte, l_counts, r_counts, l_speed, r_speed, light_bumper = Roomba.ReadQueryStream(7,43,44,42,41,45) # Read new wheel counts
 				
 				# Record the current time since the beginning of loop
 				current_time = (time.time() - time_base) # Current time of data
-				
-				# Calculate the count differences and correct for overflow
-				delta_l_count = (l_counts - l_counts_current)
-				if delta_l_count > pow(2,15): # 2^15 is somewhat arbitrary
-					delta_l_count -= pow(2,16)
-				if delta_l_count < -pow(2,15): # 2^15 is somewhat arbitrary
-					delta_l_count += pow(2,16)
-				delta_r_count = (r_counts - r_counts_current)
-				if delta_r_count > pow(2,15): # 2^15 is somewhat arbitrary
-					delta_r_count -= pow(2,16)
-				if delta_r_count < -pow(2,15): # 2^15 is somewhat arbitrary
-					delta_r_count += pow(2,16)
-				
-				delta_angle = (delta_l_count-delta_r_count)*TURN_CONSTANT
-				angle += delta_angle
-				if angle >= 360 or angle < 0:
-					angle = (angle % 360) # Normalize the angle value from [0,360)
-				# Determine what method to use to find the change in distance
-				if delta_l_count-delta_r_count == 0: # delta_angle == 0
-					delta_distance = 0.5*(delta_l_count+delta_r_count)*DISTANCE_CONSTANT
-				else: # delta_angle != 0
-					delta_distance = 2*(WHEEL_SEPARATION*(delta_l_count/(delta_l_count-delta_r_count)-.5))*math.sin(math.radians(delta_angle/2))
-				distance += delta_distance # Updated distance of Roomba
-				# Find new x and y position
-				x_pos += delta_distance*math.cos(math.radians(angle-.5*delta_angle))
-				y_pos += delta_distance*math.sin(math.radians(angle-.5*delta_angle))
 				
 				# Measure IMU values
 				accel_x, accel_y, accel_z = imu.acceleration
@@ -181,7 +127,8 @@ def MyLoop(stdscr):
 				# Display current data to the screen
 				stdscr.move(5,0)
 				stdscr.clrtoeol() # Clear line
-				stdscr.addstr(5,0,"{0:.4f}, {1:0>8b}, {2}, {3}, {4:0>8b}, {5}, {6}, {7:.3f}, {8:.3f}, {9:.2f};".format(current_time, bumper_byte, l_counts, r_counts, light_bumper, l_speed, r_speed, y_pos, x_pos, angle))
+				stdscr.addstr(5,0,"{0:.4f}, {1:0>8b}, {2}, {3}, {4:0>8b}, {5}, {6}, {7:.3f}, {8:.3f}, {9:.2f};"\
+					.format(current_time, bumper_byte, l_counts, r_counts, light_bumper, l_speed, r_speed, Roomba.Y_position, Roomba.X_position, math.degrees(Roomba.heading)))
 				stdscr.move(6,0)
 				stdscr.clrtoeol() # Clear line
 				stdscr.addstr(6,0,"{0:.3f}, {1:.3f}, {2:.3f}; (9.8 m/s^2)".format(accel_x, accel_y, accel_z))
@@ -191,15 +138,12 @@ def MyLoop(stdscr):
 				stdscr.move(8,0)
 				stdscr.clrtoeol() # Clear line
 				stdscr.addstr(8,0,"{0:.3f}, {1:.3f}, {2:.3f}; (deg/s)".format(gyro_x, gyro_y, gyro_z))
-				
-				# Update current wheel encoder counts
-				l_counts_current = l_counts
-				r_counts_current = r_counts
-			
+
+			# End if Roomba.Available
 			ctrl_char = stdscr.getch(2,0)
 			# Returns -1 if there is no input
 			if ctrl_char != -1: # If there is an input
-				stop_base = time.time()
+				#stop_base = time.time()
 				# Reset values
 				if ctrl_char == 113: # "q"
 					forward = 0
@@ -225,6 +169,7 @@ def MyLoop(stdscr):
 					spin = 150
 				elif spin < -150:
 					spin = -150
+			# End if ctrl_char
 			
 			# Update forward and spin values to the screen
 			stdscr.move(3,0)
@@ -239,17 +184,18 @@ def MyLoop(stdscr):
 		except KeyboardInterrupt:
 			print('') # print new line
 			break # exit while loop
-	
+	# End while
 	Roomba.Move(0,0) # Stop Roomba movement
 	Roomba.PauseQueryStream()
 	if Roomba.Available() > 0:
 		x = Roomba.DirectRead(Roomba.Available()) # Clear out residual Roomba data
 		#print(x) # Include for debugging purposes
-
+	# End if Roomba.Available
+# End def MyLoop
 curses.wrapper(MyLoop) # Call "MyLoop" function in the curses wrapper
 ## -- Ending Code Starts Here -- ##
 # Make sure this code runs to end the program cleanly
-#Roomba.PlaySMB()
+Roomba.PlaySMB()
 GPIO.output(gled, GPIO.LOW) # Turn off green LED
 
 curses.echo()
