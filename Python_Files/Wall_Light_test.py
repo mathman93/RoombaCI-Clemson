5c781dfdd12e1815979048ef9002572ca9e39067
Timothy Anglea <mathman93@gmail.com>
2021-10-25 16:02:44 -0400

    fixed formatting.

--- Python_Files/Wall_Light_test.py
+++ Python_Files/Wall_Light_test.py
@@ -123,7 +123,7 @@ while True:
     try:  
         if Roomba.Available()>0:
             bumper_byte, l_counts, r_counts, light_bumper, lb_ll, lb_fl, lb_cl, lb_cr, lb_fr, lb_rr = Roomba.ReadQueryStream(7, 43, 44, 45, 46, 47, 48, 49, 50, 51)
-            print("{0:.6f}, {1}, {2}, {3:.4f}, {4:0>8b}, {5:0>8b}, {6}, {7}, {8}, {9};"\
+            print("{0}, {1}, {2:0>8b}, {3:0>8b}, {4}, {5}, {6}, {7}, {8}, {9};"\
                         .format(l_counts, r_counts, bumper_byte, light_bumper, lb_ll, lb_fl, lb_cl, lb_cr, lb_fr, lb_rr))
             if bumper_byte > 0 and state == 0:
                 state = 1
