1a0156a3cc58fcedc674d737b16b368ad5245d6e
byronsull <bsulli2@clemson.edu>
2021-10-25 16:37:04 -0400

    File writing added

--- Python_Files/Wall_Light_test.py
+++ Python_Files/Wall_Light_test.py
@@ -124,9 +124,9 @@ while True:
 		if Roomba.Available()>0:
 			bumper_byte, l_counts, r_counts, light_bumper, lb_ll, lb_fl, lb_cl, lb_cr, lb_fr, lb_rr = Roomba.ReadQueryStream(7, 43, 44, 45, 46, 47, 48, 49, 50, 51)
 			Roomba.UpdatePosition(l_counts, r_counts)
-			
-			print("{0}, {1}, {2:0>8b}, {3:0>8b}, {4}, {5}, {6}, {7}, {8}, {9}; Position: X:{10:.2f} Y:{11:.2f} Head:{12:.2f}"\
-                        .format(l_counts, r_counts, bumper_byte, light_bumper, lb_ll, lb_fl, lb_cl, lb_cr, lb_fr, lb_rr, Roomba.X_position, Roomba.Y_position, Roomba.heading))
+			outString = "{0}, {1}, {2:0>8b}, {3:0>8b}, {4}, {5}, {6}, {7}, {8}, {9}; Position: X:{10:.2f} Y:{11:.2f} Head:{12:.2f}"
+			print(outString.format(l_counts, r_counts, bumper_byte, light_bumper, lb_ll, lb_fl, lb_cl, lb_cr, lb_fr, lb_rr, Roomba.X_position, Roomba.Y_position, Roomba.heading))
+			datafile.write(outString + "\n")
 			if bumper_byte > 0 and state == 0:
 				state = 1
 				base = time.time()
@@ -147,3 +147,8 @@ if Roomba.Available()>0: # If data exists in Query Stream...
 if file_create == True:
 	datafile.close() # Close data file
 # End if file_create
+
+## -- Ending Code Starts Here -- ##
+# Make sure this code runs to end the program cleanly
+Roomba.ShutDown() # Shutdown Roomba serial connection
+GPIO.cleanup() # Reset GPIO pins for next program
