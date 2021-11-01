a6686b555de46b379facb23d1cc1a118d716eeab
Bot-Justin <90235649+Bot-Justin@users.noreply.github.com>
2021-11-01 17:04:09 -0400

    Mostly finished with moveNext

--- Python_Files/Roomba_Curve_Tracking.py
+++ Python_Files/Roomba_Curve_Tracking.py
@@ -49,10 +49,34 @@ def moveNext(start,end,position):
 	next[0] = proj[0]*1.05
 	next[1] = proj[1]*1.05
 	# Roomba heading = Roomba.heading
-	# find heading based on current heading
-	# ask how what the heading is based on
-	# give speed of turn and speed of roomba based on how far roomba is from heading
-	return
+	# Calculate heading for roomba
+	magnext = math.sqrt((next[0]-position[0])**2+(next[1]-position[1])**2)
+	theta_1 = math.acos(next[0]/magnext)
+	theta = Roomba.heading
+	theta_turn = math.sin(theta_1-theta)
+	# do we want to turn left or right
+	if(theta_turn > 0.05):
+		t_dir = 1
+	elif(theta_turn < 0.05):
+		t_dir = -1
+	# used to find if roomba is going to turn before or while roomba is moving
+	theta_sb = math.cos(theta_1-theta)
+	# turn fast and move forward slow
+	if(math.pi*-7/12 < theta_sb < math.pi*5/12):
+		forwardspeed = 20
+		spinspeed = 60
+	# turn medium and move forward medium
+	elif(math.pi*-3/12 < theta_sb < math.pi*3/12):
+		forwardspeed = 40
+		spinspeed = 20
+	# do not need to turn and move forward only
+	elif(math.pi/48 < theta_sb < math.pi/48):
+		forwardspeed = 100
+	# angle is too far away and need to turn before moving
+	else:
+		spinspeed = 100
+	moveList = [forwardspeed,spinspeed*t_dir]
+	return moveList
 	
 # End futurePoint
 
@@ -73,8 +97,3 @@ def moveNext(start,end,position):
 #	if within a tolearance of that
 #	repeat above
 # 
-# have four points around the roomba of x-r,x+r,y-r,y+r
-# r is a defined constant "radius"
-# if > x+r move right if < x-r move left, if neither nothing
-# if > y+r move up if < y-r move down, if neither nothing
-# if both neither, move to next point
