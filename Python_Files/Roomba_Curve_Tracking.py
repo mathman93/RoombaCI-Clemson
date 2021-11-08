3b52f4824cc0d783f7404eeb11656c02e1b83217
Timothy Anglea <mathman93@gmail.com>
2021-11-08 15:31:36 -0500

    copied normal stuff over.

--- Python_Files/Roomba_Curve_Tracking.py
+++ Python_Files/Roomba_Curve_Tracking.py
@@ -8,6 +8,13 @@ import os.path
 import math
 import heapq
 
+## Variables and Constants ##
+global Xbee # Specifies connection to Xbee
+Xbee = serial.Serial('/dev/ttyUSB0', 115200) # Baud rate should be 115200
+# LED pin numbers
+yled = 5
+rled = 6
+gled = 13
 
 # Use dot products and unit vector to find distance along path.
 # Calculate target point a certain amount ahead of this distance along the path
@@ -88,6 +95,64 @@ def moveSpeed(theta):
 	return moveList
 # End moveSpeed
 
+## -- Code Starts Here -- ##
+# Setup Code #
+GPIO.setmode(GPIO.BCM) # Use BCM pin numbering for GPIO
+RoombaCI_lib.DisplayDateTime() # Display current date and time
+
+# LED Pin setup
+GPIO.setup(yled, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(rled, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(gled, GPIO.OUT, initial=GPIO.LOW)
+
+# Wake Up Roomba Sequence
+GPIO.output(gled, GPIO.HIGH) # Turn on green LED to say we are alive
+print(" Starting ROOMBA... ")
+Roomba = RoombaCI_lib.Create_2("/dev/ttyS0", 115200)
+Roomba.ddPin = 23 # Set Roomba dd pin number
+GPIO.setup(Roomba.ddPin, GPIO.OUT, initial=GPIO.LOW)
+Roomba.WakeUp(131) # Start up Roomba in Safe Mode
+# 131 = Safe Mode; 132 = Full Mode (Be ready to catch it!)
+Roomba.BlinkCleanLight() # Blink the Clean light on Roomba
+if Roomba.Available() > 0: # If anything is in the Roomba receive buffer
+	x = Roomba.DirectRead(Roomba.Available()) # Clear out Roomba boot-up info
+	#print(x) # Include for debugging
+# End if Roomba.Available()
+print(" ROOMBA Setup Complete")
+'''
+GPIO.output(yled, GPIO.HIGH) # Indicate within setup sequence
+# Initialize IMU
+print(" Starting IMU...")
+imu = RoombaCI_lib.LSM9DS1_I2C() # Initialize IMU
+time.sleep(0.2)
+# Clear out first reading from all sensors
+x = imu.magnetic
+x = imu.acceleration
+x = imu.gyro
+# Calibrate IMU
+print(" Calibrating IMU...")
+Roomba.Move(0,75) # Start Roomba spinning
+imu.CalibrateMag() # Calculate magnetometer offset values
+Roomba.Move(0,0) # Stop Roomba spinning
+time.sleep(0.5)
+imu.CalibrateGyro() # Calculate gyroscope offset values
+# Display offset values
+print("mx_offset = {:f}; my_offset = {:f}; mz_offset = {:f}"\
+	.format(imu.m_offset[0], imu.m_offset[1], imu.m_offset[2]))
+print("gx_offset = {:f}; gy_offset = {:f}; gz_offset = {:f}"\
+	.format(imu.g_offset[0], imu.g_offset[1], imu.g_offset[2]))
+print(" IMU Setup Complete")
+time.sleep(3) # Gives time to read offset values before continuing
+GPIO.output(yled, GPIO.LOW) # Indicate IMU setup sequence is complete
+'''
+if Xbee.inWaiting() > 0: # If anything is in the Xbee receive buffer
+	x = Xbee.read(Xbee.inWaiting()).decode() # Clear out Xbee input buffer
+	#print(x) # Include for debugging
+# End if Xbee.inWaiting()
+GPIO.output(gled, GPIO.LOW) # Indicate all set sequences are complete
+
+# Main Code #
+
 # Get the initial wheel enocder values
 [left_encoder, right_encoder] = Roomba.Query(43,44)
 Roomba.SetWheelEncoderCounts(left_encoder,right_encoder)
@@ -136,3 +201,9 @@ while True:
 #	if within a tolearance of that
 #	repeat above
 # 
+## -- Ending Code Starts Here -- ##
+# Make sure this code runs to end the program cleanly
+
+Roomba.ShutDown() # Shutdown Roomba serial connection
+Xbee.close()
+GPIO.cleanup() # Reset GPIO pins for next program
