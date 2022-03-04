4e718cdc699db9fb25db48d65f2b325d565caab1
plman5 <33044392+plman5@users.noreply.github.com>
2022-03-04 13:34:56 -0500

    Added rickroll functionality

--- Python_Files/Roomba_SongTesting.py
+++ Python_Files/Roomba_SongTesting.py
@@ -97,13 +97,9 @@ program objectives
 timestep = 8 # (1/64)ths of a second
 tone_mod = -7 # half step modulation of key
 rest = 15 - tone_mod #Rest note
-'''FullSongList = [72,2,rest,1,74,2,79,1,81,2,rest,1,79,2,rest,1,84,2,rest,1,83,2,79,1,77,2,rest,4,\
-                71,2,rest,1,74,2,77,1,83,2,rest,1,81,2,rest,1,80,2,rest,1,79,2,77,1,76,2,rest,4,\
-                72,2,rest,1,74,2,79,1,81,2,rest,1,79,2,rest,1,88,2,rest,1,86,2,84,1,81,2,rest,4,\
-                72,2,rest,1,74,2,79,1,81,2,rest,1,79,2,rest,1,88,2,rest,1,86,2,84,1,81,2,rest,4,\
-                81,2,rest,1,83,2,84,1,84,2,79,1,76,2,72,1,78,2,rest,1,77,2,rest,1,76,2,rest,4] # DK64'''
 
-FullSongList = comps.DKTest
+
+FullSongList = comps.RickRoll
 # declare vars.
 i = 0
 y = 0
