f7801e178ae23c748c09bca4dbbc03caf0ba1e73
plman5 <33044392+plman5@users.noreply.github.com>
2022-03-04 13:30:59 -0500

    Update Roomba_SongTesting.py

--- Python_Files/Roomba_SongTesting.py
+++ Python_Files/Roomba_SongTesting.py
@@ -103,7 +103,7 @@ tone_mod = -7 # half step modulation of key
                 72,2,rest,1,74,2,79,1,81,2,rest,1,79,2,rest,1,88,2,rest,1,86,2,84,1,81,2,rest,4,\
                 81,2,rest,1,83,2,84,1,84,2,79,1,76,2,72,1,78,2,rest,1,77,2,rest,1,76,2,rest,4] # DK64'''
 
-FullSongList = comps.FullSongList
+FullSongList = comps.DKTest
 # declare vars.
 i = 0
 y = 0
