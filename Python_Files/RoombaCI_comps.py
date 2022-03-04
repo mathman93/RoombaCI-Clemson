225ab8e3352920ea3dd9af7ed64aedfe1cd738e9
plman5 <33044392+plman5@users.noreply.github.com>
2022-03-04 13:26:47 -0500

    Removed extraneous text

--- Python_Files/RoombaCI_comps.py
+++ Python_Files/RoombaCI_comps.py
@@ -1,5 +1,3 @@
-Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
 
 ''' Purpose: Store compositions for use in Roomba_SongTesting'''
 
