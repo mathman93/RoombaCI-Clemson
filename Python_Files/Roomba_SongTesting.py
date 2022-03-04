56623e77dc06699db9df9f2e13ff490044e7a029
plman5 <33044392+plman5@users.noreply.github.com>
2022-03-04 13:59:42 -0500

    Update Roomba_SongTesting.py

--- Python_Files/Roomba_SongTesting.py
+++ Python_Files/Roomba_SongTesting.py
@@ -102,13 +102,13 @@ while(True):
     list = comps.Comp_dict.keys()
     if str in list:
         FullSongList = comps.Comp_dict[str]
-   ''' if str == "1":
+   ''' if str == "1"
         FullSongList = comps.Comp_dict[str]
         break
-    elif str == "2":
+    elif str == "2"
         FullSongList = comps.Comp_dict[str]
         break
-    else:
+    else
         print("Song Name not Valid")
         continue '''
 # declare vars.
